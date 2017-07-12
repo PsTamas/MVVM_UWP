a249010650ddf4febaae732e6c0a086336b8f57e
Póra-Simon Tamás <poratamas01@gmail.com>
2017-07-12 14:32:12 +0200

    Complete

--- Minimal MVVM UWP App/MainPage.xaml.cs
+++ Minimal MVVM UWP App/MainPage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using ViewModels;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,6 +26,9 @@ namespace Minimal_MVVM_UWP_App
         public MainPage()
         {
             this.InitializeComponent();
+            Organization = new OrganizationViewModel("Office");
         }
+
+        public OrganizationViewModel Organization { get; set; }
     }
 }
